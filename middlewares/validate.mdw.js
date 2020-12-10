bee748032bd104a8aec7a8c24995dbd8c424ba51
Tue Vo <vothaiminhtue0312@gmail.com>
2020-12-10 23:10:01 +0700

    fix api

--- middlewares/validate.mdw.js
+++ middlewares/validate.mdw.js
@@ -1,8 +1,13 @@
 const ajv = require('ajv');
 const httpStatus = require('http-status');
+const validator = new ajv({ allErrors: true });
+
+const dateTimeRegex = new RegExp('^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])');
+validator.addFormat('YYYY-MM-DD', {
+  validate: (dateTimeString) => dateTimeRegex.test(dateTimeString)
+});
 
 module.exports = schema => (req, res, next) => {
-  const validator = new ajv({ allErrors: true });
   const fn_validate = validator.compile(schema);
   const is_valid = fn_validate(req.body);
   if (!is_valid) {
